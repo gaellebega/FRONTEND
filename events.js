5c736241982f1c71db0ccb02cbcfdc38074d5cd2
gaellebega <gaellebega@gmail.com>
2026-06-15 10:38:58 +0200

    keyup and keydown events

--- events.js
+++ events.js
@@ -3,3 +3,25 @@ const seeing=document.getElementById("result")
 button.addEventListener("click",()=>{
   seeing.textContent="Button Clicked";
 });
+
+
+const input=document.getElementById("inputBox")
+const outputBox=document.getElementById("output")
+
+input.addEventListener("keydown",(e)=>{
+  outputBox.textContent="you pressed"+e.key;
+});
+
+
+
+
+
+
+
+
+
+const themessage=document.getElementById("mymessage");
+const theout=document.getElementById("result")
+themessage.addEventListener("keyup",(e)=>{
+  console.log("keyup:",e.key);
+})
