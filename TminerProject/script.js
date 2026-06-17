d1c3eecad889060125c91530dc568c9320d874e7
gaellebega <gaellebega@gmail.com>
2026-06-17 11:53:24 +0200

    my timer app is now alive

--- TminerProject/script.js
+++ TminerProject/script.js
@@ -2,7 +2,8 @@ let hours=0;
 let minutes=0;
 let seconds=0;
 let myInterval=null;
-setInterval(()=>{
+// this allows everything to work together so when seconds are 60 then we get to min when minutes are 60 then add again 
+function updateTimer(){
   seconds++;
   if(seconds===60){
     seconds=0;
@@ -12,22 +13,19 @@ setInterval(()=>{
     minutes=0;
     hours++;
   }
+
   document.getElementById("hours").textContent=
   String(hours).padStart(2,"0");
+
   document.getElementById("minutes").textContent=
   String(minutes).padStart(2,"0");
+
   document.getElementById("seconds").textContent=
   String(seconds).padStart(2,"0");
-},1000);
+};
 
-//STOP THE TIMER
 
-document.getElementsById("stop").addEventListener("click",()=>{
-  clearInterval(myInterval);
-  myInterval=null;
-});
 // start the timer
-
 document.getElementById("start").addEventListener("click",()=>{
   // this means if my interval is emapty or equal to the null
   if(!myInterval){
@@ -35,9 +33,15 @@ document.getElementById("start").addEventListener("click",()=>{
   }
 })
 
+
+//STOP THE TIMER
+
+document.getElementById("stop").addEventListener("click",()=>{
+  clearInterval(myInterval);
+  myInterval=null;
+});
+
 //RESET THE timer
-
-
 document.getElementById("reset").addEventListener("click",()=>{
   clearInterval(myInterval);
   myInterval=null;
@@ -48,5 +52,13 @@ document.getElementById("reset").addEventListener("click",()=>{
 
   document.getElementById("hours").textContent="00";
   document.getElementById("minutes").textContent="00";
-  document.getElementById("hours").textContent="00"
+  document.getElementById("seconds").textContent="00"
+});
+
+// RESUME
+
+document.getElementById("resume").addEventListener("click",()=>{
+  if(!myInterval){
+    myInterval=setInterval(updateTimer,1000)
+  }
 });
