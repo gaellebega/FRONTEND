6e38a4266397216fe03eedf039758ab0e4f1efb8
gaellebega <gaellebega@gmail.com>
2026-06-30 19:42:59 +0200

    using async to fetch data for api

--- CRUD/WEATHER/weather.js
+++ CRUD/WEATHER/weather.js
@@ -1,2 +1,6 @@
 const apiKey="1234567890abcdef1234567890abcdef";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=YOUR_API_KEY&units=metric"
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=YOUR_API_KEY&units=metric";
+async function checkWeather(){
+  const response=await fetch(apiUrl+`&appid=${apiKey}`);
+}
+// you can use the async instead of then to fetch as the method that is used at the above exaple
