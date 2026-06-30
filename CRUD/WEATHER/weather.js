c93c849e7d17f3065079b0b437d9e514df51bafd
gaellebega <gaellebega@gmail.com>
2026-06-30 23:23:08 +0200

    get fetched data

--- CRUD/WEATHER/weather.js
+++ CRUD/WEATHER/weather.js
@@ -1,6 +1,12 @@
+// these are ids so that we have the unique key
 const apiKey="1234567890abcdef1234567890abcdef";
+// secureconnection https/servername/query parameter which end at ? q stands for query q=kigali which means gimme the weather of keigali appid this have tosend my api
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=YOUR_API_KEY&units=metric";
 async function checkWeather(){
   const response=await fetch(apiUrl+`&appid=${apiKey}`);
+  var data=await response.json()
+  console.log(data)
 }
+checkWeather()
+// the server sends the data in JSAON FORMAT
 // you can use the async instead of then to fetch as the method that is used at the above exaple
