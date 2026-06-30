49dcb2af5d67f4fa0a98553ec525236a7b9ee68d
gaellebega <gaellebega@gmail.com>
2026-06-30 23:38:27 +0200

    error 401 solved

--- CRUD/WEATHER/weather.js
+++ CRUD/WEATHER/weather.js
@@ -1,7 +1,7 @@
 // these are ids so that we have the unique key
 const apiKey="1234567890abcdef1234567890abcdef";
 // secureconnection https/servername/query parameter which end at ? q stands for query q=kigali which means gimme the weather of keigali appid this have tosend my api
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=YOUR_API_KEY&units=metric";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&units=metric";
 async function checkWeather(){
   const response=await fetch(apiUrl+`&appid=${apiKey}`);
   var data=await response.json()
