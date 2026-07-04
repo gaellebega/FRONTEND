25557a3c9b5e537b5b9781462d84a7c9cff8270e
gaellebega <gaellebega@gmail.com>
2026-07-04 23:20:40 +0200

    readystatechange for request

--- asynchronous.js
+++ asynchronous.js
@@ -1,1 +1,8 @@
+    // the way the request object is created
 const request=new XMLHttpRequest();
+// what we nend to make the request we need 2 things
+// we need the request(GET,PUT,POST) and another argument of the end point
+request.open("GET","https://jsonplaceholder.typicode.com/todos/");
+// The above we have told our request type is and where is is going to get the data from
+//to send the request
+request.send();
