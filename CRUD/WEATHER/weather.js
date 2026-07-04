8d54705a15a4a8e64a9f3a6b53a005da7f95ced7
gaellebega <gaellebega@gmail.com>
2026-07-04 22:36:23 +0200

    creating the https request

--- CRUD/WEATHER/weather.js
+++ CRUD/WEATHER/weather.js
@@ -1,7 +1,8 @@
 // these are ids so that we have the unique key
 const apiKey="1234567890abcdef1234567890abcdef";
+const city="kigali";
 // secureconnection https/servername/query parameter which end at ? q stands for query q=kigali which means gimme the weather of keigali appid this have tosend my api
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Kigali&units=metric";
+const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
 async function checkWeather(){
   const response=await fetch(apiUrl+`&appid=${apiKey}`);
   var data=await response.json()
