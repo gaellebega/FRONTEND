4679e2d31662d4f9a259559442a7fc2590f4c9ba
gaellebega <gaellebega@gmail.com>
2026-07-06 23:14:38 +0200

    js asynchronous

--- asynchronous.js
+++ asynchronous.js
@@ -1,8 +1,12 @@
-    // the way the request object is created
-const request=new XMLHttpRequest();
-// what we nend to make the request we need 2 things
-// we need the request(GET,PUT,POST) and another argument of the end point
-request.open("GET","https://jsonplaceholder.typicode.com/todos/");
-// The above we have told our request type is and where is is going to get the data from
-//to send the request
+//JSON DATA
+const request = new XMLHttpRequest();
+request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.responseText);
+    }
+    else if (request.readyState === 4) {
+        console.log("Could not fetch the data!")
+    }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/?t=" + new Date().getTime());
 request.send();
