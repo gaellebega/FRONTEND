56f6badb83007aed59b0ee0836907dbf539b696b
gaellebega <gaellebega@gmail.com>
2026-07-23 21:34:51 +0200

    asynchronous wait

---
+++ async.js
@@ -0,0 +1,7 @@
+// to be able to use the asynch we have ti write async keyword then the async always have to return the promise
+//this is called the async function
+const getTodos=async()=>{
+
+};
+const test=getTodos();
+console.log(test)
--- fetch.js
+++ fetch.js
@@ -1,26 +1,32 @@
 //  fetching is js function to request data from a server or API
 //  fetch(url)
 // the response contains something like status,headers,ok,body
- fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response)=>{
-        //we use return because we want to pass the result to the next .then()
-        //we use the response.json to be able to read the body
-        return response.json();
-      })
-      .then((data)=>{
-        //here to output the error we are getting
-        console.log(data);
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    //we use return because we want to pass the result to the next .then()
+    //we use the response.json to be able to read the body
+    return response.json();
+  })
+  .then((data) => {
+    //here to output the error we are getting
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 //promise is only rejected when we got some network error but the status willl be 404
 //they are not using the json because they dont wanna see the data only to check the response
-  fetch("todos/damy.json") 
-        .then((response)=>{
-         console.log("resolved",response)
-        })
-        .catch((err)=>{
-         console.log("rejected",err)
-        }) 
+fetch("todos/damy.json")
+  .then((response) => {
+    console.log("resolved", response)
+    //this method will get us data
+    return response.json();
+  })
+  .then((data) => {
+    //now you can see the data
+    console.log(data);
+  })
+    .catch((err) => {
+      console.log("rejected", err)
+    })
 
