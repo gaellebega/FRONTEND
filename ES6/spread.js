f710704f3593a3a4aee6d263d7f7a87b6ea9b6c5
gaellebega <gaellebega@gmail.com>
2026-07-25 23:02:42 +0200

    spread operator in ES6

---
+++ ES6/spread.js
@@ -0,0 +1,13 @@
+var nums1=[1,2,3];
+// spread operator usage
+var nums2=[...nums1,4,5,6];
+console.log(nums1)
+
+
+//Hpw can we add the numbers using the spread operators
+var numbers=[10,20,30]
+function addNums(a,b,c){
+  console.log(a+b+c);
+}
+//this means take all the numbers which are there in the numbers array
+addNums(...numbers)
